4bed748ebf1debcf7030a70b3be36a70f097cf50
swill <swilltec@gmail.com>
2021-05-09 10:48:21 +0000

    feat: home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,71 +7,76 @@ export default class Home extends Component {
     return (
       <>
         <Header page="home" />
-        <main className="ui middle aligned center aligned grid">
-          <div className="column home">
-            <div className="ui secondary pointing menu">
-              <a className="item active">Unanswered Questions</a>
-              <a className="item">Answered Questions</a>
-            </div>
-            <div className="ui segment">
-              <div class="ui cards ">
-                <div class="card">
-                  <div class="content">
-                    <div class="header">Elliot Fu</div>
-                    <div class="description">
-                      Elliot Fu is a film-maker from New York.
-                      Elliot Fu is a film-maker from New York.
-                      Elliot Fu is a film-maker from New York.
-                      Elliot Fu is a film-maker from New York.
+        <main className="home">
+          <div className="container mt-5">
+            <div className="card p-2">
+              <nav className="">
+                <div class="nav nav-tabs" id="nav-tab" role="tablist">
+                  <button
+                    class="nav-link active"
+                    id="nav-answered-tab"
+                    data-bs-toggle="tab"
+                    data-bs-target="#nav-answered"
+                    type="button"
+                    role="tab"
+                    aria-controls="nav-answered"
+                    aria-selected="true"
+                  >
+                    Answered
+                  </button>
+                  <button
+                    class="nav-link"
+                    id="nav-unanswered-tab"
+                    data-bs-toggle="tab"
+                    data-bs-target="#nav-unanswered"
+                    type="button"
+                    role="tab"
+                    aria-controls="nav-unanswered"
+                    aria-selected="false"
+                  >
+                    Unanswered
+                  </button>
+                </div>
+              </nav>
+              <div class="tab-content mt-2" id="nav-tabContent">
+                <div
+                  class="tab-pane fade show active"
+                  id="nav-answered"
+                  role="tabpanel"
+                  aria-labelledby="nav-answered-tab"
+                >
+                  <div class="card">
+                    <div class="card-header text-center">User ask</div>
+                    <div class="row g-0">
+                      <div className="col-3 pt-1">
+                        <img
+                          src="https://randomuser.me/api/portraits/women/52.jpg"
+                          alt="..."
+                        />
+                      </div>
+                      <div className="col-8 offset-1">
+                        <div class="card-body">
+                         
+                          <p class="card-text">
+                            With supporting text below as a natural lead-in to
+                            additional content.
+                          </p>
+                          <a href="#" class="btn btn-primary">
+                            View Poll
+                          </a>
+                        </div>
+                      </div>
                     </div>
                   </div>
-                  <div class="ui bottom attached button">
-                    <i class="add icon"></i>
-                    Add Friend
-                  </div>
                 </div>
-                <div class="card">
-                  <div class="content">
-                    <div class="header">Veronika Ossi</div>
-                    <div class="description">
-                      Veronika Ossi is a set designer living in New York who
-                      enjoys kittens, music, and partying.
-                    </div>
-                  </div>
-                  <div class="ui bottom attached button">
-                    <i class="add icon"></i>
-                    Add Friend
-                  </div>
+                <div
+                  class="tab-pane fade"
+                  id="nav-unanswered"
+                  role="tabpanel"
+                  aria-labelledby="nav-unanswered-tab"
+                >
+                  ...
                 </div>
-                
-                <div class="card">
-                  <div class="content">
-                    <div class="header">Jenny Hess</div>
-                    <div class="description">
-                      Jenny is a student studying Media Management at the New
-                      School
-                    </div>
-                  </div>
-                  <div class="ui bottom attached button">
-                    <i class="add icon"></i>
-                    Add Friend
-                  </div>
-                </div>
-
-                <div class="card">
-                  <div class="content">
-                    <div class="header">Jenny Hess</div>
-                    <div class="description">
-                      Jenny is a student studying Media Management at the New
-                      School
-                    </div>
-                  </div>
-                  <div class="ui bottom attached button">
-                    <i class="add icon"></i>
-                    Add Friend
-                  </div>
-                </div>
-                
               </div>
             </div>
           </div>
