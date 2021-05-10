ab24090eab61e8498b8a39fe10b20d7ca7c6b909
swill <swilltec@gmail.com>
2021-05-10 02:22:16 +0000

    feat: add reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,10 @@
 import { combineReducers } from "redux";
-
+import authUser from "../reducers/authUser";
+import questions from "./question";
+import users from "../reducers/users";
 
 export default combineReducers({
-    users: 'Test user',
+  authUser,
+  questions,
+  users,
 });
